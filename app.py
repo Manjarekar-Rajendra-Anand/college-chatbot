b692b3737fd3b4375f1a22b6ef68e1464102a772
Manjarekar-Rajendra-Anand <ce24f035@ybit.ac.in>
2026-03-28 21:03:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ def chatbot_response():
     response_text = get_response(user_input)
     print(f"Responding with: {response_text}")
     return jsonify({"response": response_text})
-
+import os
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
